1b420c25cd2fa3368dca52fce513a9cff50d2103
fatush254 <fgalgalo945@gmail.com>
2024-04-15 00:35:07 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+// index.js
+
+function writeCards(names, eventName) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+module.exports = {
+    writeCards,
+    countDown
+};
